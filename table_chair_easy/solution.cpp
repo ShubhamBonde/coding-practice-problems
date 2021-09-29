874aab22097790408d8c266b76c623e357c44080
Shubham Bonde <81147655+ShubhamBonde@users.noreply.github.com>
2021-09-29 09:50:09 +0530

    Update solution.cpp

--- table_chair_easy/solution.cpp
+++ table_chair_easy/solution.cpp
@@ -13,8 +13,8 @@ int main()
 		int rem = m % 4;
 
 		if (!rem) cout << 0 << endl;
-		if (rem == 1) cout << min(r3, min(r2+r1, r1*3)) << endl;
-		if (rem == 2) cout << min(r2, min(r1+r2, r3+r2)) << endl;
-		if (rem == 3) cout << min(r1, min(r2+r3, r3*3)) << endl;
+		if (rem == 1) cout << min(r3, min(r2+r1, r1+r1+r1)) << endl;
+		if (rem == 2) cout << min(r2, min(r1+r1, r3+r3)) << endl;
+		if (rem == 3) cout << min(r1, min(r2+r3, r3+r3+r3)) << endl;
 	}
 }
